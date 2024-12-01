c4341056103acdbfc20cc60c84398f1bb010041b
Christian Zöller <No3x@users.noreply.github.com>
2024-12-01 12:47:03 +0100

    add solution

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day1/day1.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day1/day1.kt
@@ -1,9 +1,11 @@
 package de.no3x.adventofcode.twentyfour.day1
 
+import kotlin.math.abs
+
 class Day1 {
-    fun distance(list1: List<Int>, list2: List<Int>): Any {
-        return 11
+    fun distance(list1: List<Int>, list2: List<Int>): Int {
+        return list1.sorted().zip(list2.sorted()).sumOf {
+            abs(it.first - it.second)
+        }
     }
-
-
 }
