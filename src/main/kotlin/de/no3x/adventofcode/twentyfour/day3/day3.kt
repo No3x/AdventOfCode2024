bb1946d2f92370c280894fcca218a9ccc589f0f5
Christian Zöller <No3x@users.noreply.github.com>
2024-12-03 19:48:12 +0100

    Adapted regex because it matched too much fuzzy input that should not be matched

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
@@ -4,21 +4,19 @@ class Day3 {
 
     fun solve(input: String): Int {
 
-        var muls = parseMuls(input);
+        val muls = parseMuls(input)
         return muls.map {
-            mul -> mul.x * mul.y
+            mul -> mul.first * mul.second
         }.sum()
     }
 
-    private fun parseMuls(input: String): List<Mul> {
-        val regex = """(mul\([^\d),]?(\d+)[^),]?,[^),]?(\d+)[^\d),]?\))""".toRegex()
+    private fun parseMuls(input: String): List<Pair<Int,Int>> {
+        val regex = """(mul\((\d+),(\d+)\))""".toRegex()
         val matchResults = regex.findAll(input)
         return matchResults.map {
-            matchResult -> Mul(Integer.parseInt(matchResult.groups[2]!!.value), Integer.parseInt(matchResult.groups[3]!!.value))
+            matchResult -> Pair(Integer.parseInt(matchResult.groups[2]!!.value), Integer.parseInt(matchResult.groups[3]!!.value))
         }.toList()
     }
 
 }
-
-data class Mul(val x: Int, val y: Int)
 
