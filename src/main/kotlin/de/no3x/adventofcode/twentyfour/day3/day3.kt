ab22597e36d80345bef7bf825772a78b93e52204
Christian Zöller <No3x@users.noreply.github.com>
2024-12-03 19:59:26 +0100

    refactoring

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
@@ -5,16 +5,17 @@ class Day3 {
     fun solve(input: String): Int {
 
         val muls = parseMuls(input)
-        return muls.map {
-            mul -> mul.first * mul.second
-        }.sum()
+        return muls.sumOf { mul ->
+            mul.first * mul.second
+        }
     }
 
-    private fun parseMuls(input: String): List<Pair<Int,Int>> {
-        val regex = """(mul\((\d+),(\d+)\))""".toRegex()
-        val matchResults = regex.findAll(input)
-        return matchResults.map {
-            matchResult -> Pair(Integer.parseInt(matchResult.groups[2]!!.value), Integer.parseInt(matchResult.groups[3]!!.value))
+    private fun parseMuls(input: String): List<Pair<Int, Int>> {
+        val regex = """mul\((\d+),(\d+)\)""".toRegex()
+        return regex.findAll(input).map { matchResult ->
+            matchResult.destructured.let { (first, second) ->
+                first.toInt() to second.toInt()
+            }
         }.toList()
     }
 
