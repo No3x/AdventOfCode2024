a1915afa542904635ad67e9805f4016ccb2367cb
Christian Zöller <No3x@users.noreply.github.com>
2024-12-03 19:26:16 +0100

    Day3 passing test

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day3/day3.kt
@@ -3,8 +3,22 @@ package de.no3x.adventofcode.twentyfour.day3
 class Day3 {
 
     fun solve(input: String): Int {
-        return 1
+
+        var muls = parseMuls(input);
+        return muls.map {
+            mul -> mul.x * mul.y
+        }.sum()
+    }
+
+    private fun parseMuls(input: String): List<Mul> {
+        val regex = """(mul\([^\d),]?(\d+)[^),]?,[^),]?(\d+)[^\d),]?\))""".toRegex()
+        val matchResults = regex.findAll(input)
+        return matchResults.map {
+            matchResult -> Mul(Integer.parseInt(matchResult.groups[2]!!.value), Integer.parseInt(matchResult.groups[3]!!.value))
+        }.toList()
     }
 
 }
+
+data class Mul(val x: Int, val y: Int)
 
