30ff86b5b9d7e62edb8cf84ab45c8926e519c724
Christian Zöller <No3x@users.noreply.github.com>
2024-12-04 19:03:58 +0100

    Working day4

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day4/day4.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day4/day4.kt
@@ -2,19 +2,63 @@ package de.no3x.adventofcode.twentyfour.day4
 
 class Day4 {
 
-    fun solve(input: String): Int {
+    fun solve(matrix: List<List<Char>>): Int {
+        val chars = mutableListOf<Char>()
+        var currentMatrix = matrix
+        repeat(4) {
+            val currentMatrixChars : List<Char> = currentMatrix.flatMap { it + '|' }
+            val segmentChars = collectDiagonally(currentMatrix)
+            chars.addAll(segmentChars.toList())
+            chars.addAll(currentMatrixChars)
+            val rotated = rotateMatrix90CW(currentMatrix)
+            currentMatrix = rotated
+        }
 
-       return 1;
+        return countOccurrences(chars.joinToString(""), "XMAS")
     }
+}
 
-    private fun parseMuls(input: String): List<Pair<Int, Int>> {
-        val regex = """mul\((\d+),(\d+)\)""".toRegex()
-        return regex.findAll(input).map { matchResult ->
-            matchResult.destructured.let { (first, second) ->
-                first.toInt() to second.toInt()
+inline fun <reified T> List<List<T>>.to2DArray(): Array<Array<T>> {
+    return this.map { it.toTypedArray() }.toTypedArray()
+}
+
+fun countOccurrences(str: String, sub: String): Int {
+    return str.windowed(sub.length) { it == sub }.count { it }
+}
+
+inline fun <reified T> rotateMatrix90CW(matrix: List<List<T>>): List<List<T>> {
+    val n = matrix.size
+    // Create a new list of lists with the same size, filled with the first element
+    val rotated = List(n) { MutableList(n) { matrix[0][0] } }
+
+    for (i in matrix.indices) {
+        for (j in matrix[i].indices) {
+            rotated[j][n - 1 - i] = matrix[i][j]
+        }
+    }
+    return rotated
+}
+
+fun collectDiagonally(chars: List<List<Char>>): CharArray {
+    val ans: MutableList<Char> = ArrayList()
+    val queue = ArrayDeque<Iterator<Char>>()
+    var pos = 0
+    do {
+        if (pos < chars.size) {
+            queue.addFirst(chars[pos].iterator())
+        }
+        var sz = queue.size
+        while (--sz >= 0) {
+            val cur = queue.removeFirst()
+            ans.add(cur.next())
+            if (cur.hasNext()) {
+                queue.addLast(cur)
             }
-        }.toList()
-    }
-
+        }
+            // add any char as separator so words do not chain across diagonals
+            ans.add('|')
+        pos++
+    } while (queue.isNotEmpty() || pos < chars.size)
+    return ans.toCharArray()
 }
 
--- src/test/kotlin/de/no3x/adventofcode/twentyfour/day4/Day4Test.kt
+++ src/test/kotlin/de/no3x/adventofcode/twentyfour/day4/Day4Test.kt
@@ -10,10 +10,28 @@ import org.junit.jupiter.api.Test
 class Day4Test {
 
     @Test
-    fun `sample data mul to 161`(
+    fun `test rotate diagonal`() {
+        val input =
+            listOf(
+                listOf('X', '.', '.', '.'),
+                listOf('.', 'M', '.', '.'),
+                listOf('.', '.', 'A', '.'),
+                listOf('.', '.', '.', 'S')
+            )
+
+        val result = Day4().solve(input)
+
+        assertThat(result).isEqualTo(1)
+    }
+
+
+    @Test
+    fun `sample data has 18 occurrences`(
         @GivenTextResource("de/no3x/adventofcode/twentyfour/day4/sample.csv") input: String
     ) {
-        val result = Day4().solve(input)
+        val matrix = toMatrix(input)
+
+        val result = Day4().solve(matrix)
 
         assertThat(result).isEqualTo(18)
     }
@@ -23,9 +41,18 @@ class Day4Test {
     fun `puzzle data input`(
         @GivenTextResource("de/no3x/adventofcode/twentyfour/day4/puzzle.csv") input: String
     ) {
-        val result = Day4().solve(input)
+        val matrix = toMatrix(input)
+
+        val result = Day4().solve(matrix)
 
         println(result)
         assertThat(result).isNotNull()
+    }
+
+    private fun toMatrix(input: String): List<List<Char>> {
+        return input.lines()
+            .map { s ->
+                s.map { it }
+            }
     }
 }
