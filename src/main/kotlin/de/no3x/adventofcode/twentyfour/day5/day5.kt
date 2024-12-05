c0cbcedcde7ed72f9756e4842b7d3b7f25f2338b
Christian Zöller <No3x@users.noreply.github.com>
2024-12-05 21:04:46 +0100

    solution day5

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day5/day5.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day5/day5.kt
@@ -1,13 +1,44 @@
 package de.no3x.adventofcode.twentyfour.day5
 
+import kotlin.math.ceil
+
 data class Input(
-    val rules: List<Pair<Int, Int>>,
-    val updates: List<List<Int>>
+    val rules: List<Pair<Int, Int>>, val updates: List<List<Int>>
 )
 
 class Day5 {
 
     fun solve(input: Input): Int {
-        return input.rules.size
+
+        val result = input.updates.filterIndexed { index, update ->
+            val isValid = isValidUpdate(update, input.rules)
+            println("Update ${index + 1}: ${if (isValid) "Valid" else "Invalid"}")
+            isValid
+        }.sumOfTheirMiddleElement()
+
+        return result
+    }
+
+    private fun isValidUpdate(update: List<Int>, rules: List<Pair<Int, Int>>): Boolean {
+        val pageIndex = update.withIndex().associate { it.value to it.index }
+        return rules.all { (before, after) ->
+            when {
+                before !in pageIndex || after !in pageIndex -> true // Ignore rules with missing pages
+                else -> pageIndex[before]!! < pageIndex[after]!!
+            }
+        }
+    }
+
+
+}
+
+private fun List<List<Int>>.sumOfTheirMiddleElement(): Int {
+    return this.sumOf {
+        val size = it.size
+        assert(size > 2) {
+            "$it is too short"
+        }
+        val middle = if ((size % 2) == 0) size / 2 else ceil(size / 2f).toInt()
+        it[middle - 1]
     }
 }
