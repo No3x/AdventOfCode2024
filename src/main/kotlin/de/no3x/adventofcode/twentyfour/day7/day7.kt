c1b679f6794bf7e38862f650dd37f30d77f9b58a
Christian Zöller <No3x@users.noreply.github.com>
2024-12-09 23:43:41 +0100

    day7 part2

--- src/main/kotlin/de/no3x/adventofcode/twentyfour/day7/day7.kt
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day7/day7.kt
@@ -2,9 +2,6 @@ package de.no3x.adventofcode.twentyfour.day7
 
 
 
-enum class Operation { MUL, ADD }
-
-
 class Day7 {
 
     fun solve(input: List<String>): Long {
@@ -41,6 +38,47 @@ class Day7 {
         } else {
             allEquations(head + list.first(), list.drop(1)) +
             allEquations(head * list.first(), list.drop(1))
+        }
+    }
+}
+
+class Day7Part2 {
+
+    fun solve(input: List<String>): Long {
+        val x = input
+            .flatMap { parseMuls(it) }
+            .mapNotNull { bruteForce(it) }
+            .sum()
+        println(x)
+        return x
+    }
+
+    private fun parseMuls(input: String): List<Pair<Long, List<Long>>> {
+        val result = input.substringBefore(":")
+        val ops = input.substringAfter(":")
+        return listOf(
+            result.toLong() to parseNums(ops)
+        )
+
+    }
+
+    fun parseNums(input: String): List<Long> {
+        return input.trim().split(" ").map { it.toLong() }
+    }
+
+    private fun bruteForce(input: Pair<Long, List<Long>>): Long? {
+        val numbers = input.second
+
+        return input.second.filter { allEquations(numbers.first(), numbers.drop(1)).any { input.first == it } }.map {input.first}.firstOrNull()
+    }
+
+    private fun allEquations(head: Long, list: List<Long>): List<Long> {
+        return if (list.isEmpty()) {
+            listOf(head)
+        } else {
+            allEquations(head + list.first(), list.drop(1)) +
+                    allEquations(head * list.first(), list.drop(1)) +
+                    allEquations("$head${list.first()}".toLong(), list.drop(1))
         }
     }
 }
--- src/test/kotlin/de/no3x/adventofcode/twentyfour/day7/Day7Test.kt
+++ src/test/kotlin/de/no3x/adventofcode/twentyfour/day7/Day7Test.kt
@@ -28,4 +28,14 @@ class Day7Test {
         println(result)
         assertThat(result).isNotNull()
     }
+
+    @Test
+    fun `puzzle data input part2`(
+        @GivenTextResource("de/no3x/adventofcode/twentyfour/day7/puzzle.csv") input: String
+    ) {
+        val result = Day7Part2().solve(input.lines())
+
+        println(result)
+        assertThat(result).isEqualTo(61561126043536)
+    }
 }
