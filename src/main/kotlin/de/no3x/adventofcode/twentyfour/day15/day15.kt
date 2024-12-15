c259d0c062b177509bb3a9b31e59be889047f8c1
Christian Zöller <No3x@users.noreply.github.com>
2024-12-15 11:54:53 +0100

    day15 scaffolding

---
+++ src/main/kotlin/de/no3x/adventofcode/twentyfour/day15/day15.kt
@@ -0,0 +1,11 @@
+package de.no3x.adventofcode.twentyfour.day15
+
+
+class Day15 {
+
+    fun solve(input: List<String>): Long {
+
+        return 14
+    }
+
+}
---
+++ src/test/kotlin/de/no3x/adventofcode/twentyfour/day15/Day15Test.kt
@@ -0,0 +1,32 @@
+package de.no3x.adventofcode.twentyfour.day15
+
+import io.hosuaby.inject.resources.junit.jupiter.GivenTextResource
+import io.hosuaby.inject.resources.junit.jupiter.TestWithResources
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+@TestWithResources
+class Day15Test {
+
+    @Test
+    fun `sample is 2028`(
+        @GivenTextResource("de/no3x/adventofcode/twentyfour/day15/sample.csv") input: String
+    ) {
+        val result = Day15().solve(input.lines())
+
+        assertThat(result).isEqualTo(2028)
+    }
+
+
+    @Test
+    fun `puzzle data input`(
+        @GivenTextResource("de/no3x/adventofcode/twentyfour/day15/puzzle.csv") input: String
+    ) {
+        val result = Day15().solve(input.lines())
+
+        println(result)
+        assertThat(result).isNotNull()
+    }
+
+}
